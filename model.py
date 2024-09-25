ac5e484c451c038dd68ff94d568911237d653e4b
pirateking22017 <amanx.432.kp@gmail.com>
2024-09-26 00:22:06 +0530

    self attention mechanism ++ entire encoder structure implmented -- will do decoder next

--- model.py
+++ model.py
@@ -15,10 +15,9 @@ class InputEmbeddings(nn.Module):
     
     #next module we are gonna build is the positional encoding part 
 
-"""
-Our org sentence gets mapped to the embeddigns layer
-we wanna convey the info abt the position of each word inside the sentence this i done by adding another vector of the same size by a formula
-this tells the model - this word occupies this pos in the sentence"""
+
+# Our org sentence gets mapped to the embeddigns layer we wanna convey the info abt the position of each word inside the sentence this i done by adding another vector of 
+# the same size by a formula this tells the model - this word occupies this pos in the sentence
 class PostionalEncoding(nn.Module):
     def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
         #dmodel fo the size of vector, seq lgt for the max lgt of sentence and we want to create one vector for each position, 
@@ -125,7 +124,21 @@ class MultiHeadAttentionBlock(nn.Module):
     @staticmethod
     def attention(query, key, value, mask, dropout: nn.Dropout):
         d_k = query.shape[-1]
-
+        # scaled dot product attention 3.2.1 in the paper
+        # @ refer to matrix multp in pytorch , tranpose (-2,-1) refers to transpose the last to dimensions which converts this 
+        # (batch, h, seq_len, d_k) --> (batch, h, d_k, seq_len ) {intermediate step, final is below}
+        # dimension == (batch, h, seq_len, d_k) --> (batch, h, seq_len, d_k)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        #b4 we apply softmax, apply the mask -- replace the values with vv smal valuees
+        if mask is not None:
+            attention_scores.masked.fill_(mask == 0, -1e9) #wherer mask == 0, replace 0 with -1e9 which is a vv small value 
+            #we dont want some word to watch future words, or in decoder or the filler/ padding to interact with the "real" words
+        attention_scores =  attention_scores.softmax(dim = -1) # dimension ==  (batch, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        #multp o/p of softmax with v -- this'll be a matrix multiplication
+        return (attention_scores @ value), attention_scores #used for visalization as well
+    
     #now we implment the mask as well -- siccnce we want some words to not interact with otehrs -- particularly here we dont want old words to know abt future words 
     #we want them to know abt prev words but not future words
     #so we repalce their attention score to epsilon -- eps or osmething vv small -- so that their softmax will become 0 since its e^x where x= -infinity ==> e= vv close to 0
@@ -142,7 +155,66 @@ class MultiHeadAttentionBlock(nn.Module):
         #do same for key and value
         key =  key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2) #dimensions == same as above -- same as query 
         value =  value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2) #dimensions == same as above
+        #implement softmax function -- refer paper for more details
+        x, self.attention_socres =  MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+        #peform conatenation acc to paper
+        
+        x = x.tranpose(1,2).contiguous().view(x.shape[0], -1, self.h*self.d_k) #dimension == (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+        # contiguous why ??
 
+        return self.w_o(x) #dimensions == (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+    
+
+#this skip conn is like bw 1. pe and lower add&norm in encoder -- skips multihead 2. lower add&norm and upper add&norm -- skips ff (feedFwd) 
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalzation
+    
+    def forward(self, x, sublayer): #we take x and combine it with the ouput of the prev layer -- this is for skip conn no.2 -- refer this class comments
+        return x + self.dropout(sublayer(self.norm(x))) # the guide i followed made a slight change -- paper first applies sublayer then applies normalization -- ive reversed it
+
+
+"""
+now we will be create the encoder block 
+shopping list:
+1x multihead
+2x add&norm
+1x feedFwd
+"""
+
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout : float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+    
+    def forward(self, x, src_mask):
+        #src_mask is for theinput of the encoder -- we dont want padding word to interact with other words -- sowe have to aply the src mask
+        # first x is from after the pe step -- then the next x has to be passed thru the multi head attention b4 being pased to the residual connection (else called as add&norm)
+        # lambda is just a one line finc so taht i dont have to initilize it with a name :p
+        # after we pass the two -- wehve to combine as well
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x,src_mask)) #its k/a self attention coz role of q,k,v is  itself 
+        # -- each word of the senetence is interacting with other words fo the same sentence -- not true for decoder 
+        # -- there we have cross attention -- query from decoder is watching the key and value from encoder
+        # this is the SECOND residual or SKIP CONN -- refer shopping list -- refer paper dgm
+        x = self.residual_connections[1](x, lambda x: self.feed_forward_block) 
+        #combine the two
+        return x
+    
+#since encoder can have n number of encoder blocks
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalzation()
+    
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask) #o/p of current layer bcomes i/p of next layer
+        return self.norm(x)
 
 
 
